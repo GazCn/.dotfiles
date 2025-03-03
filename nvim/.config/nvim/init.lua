b6c104a73969db30f9e8c0bdbf7d6fb010f72325
GazCn <garycaen@gmail.com>
2025-03-03 20:42:19 +0000

    updated things

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -43,3 +43,13 @@ vim.schedule(function()
 end)
 vim.opt.clipboard = "unnamedplus"
 vim.opt.scrolloff = 20
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
